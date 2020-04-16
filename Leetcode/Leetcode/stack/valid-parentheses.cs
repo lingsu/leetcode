d57e0c86f045c0b344411fb9287715a007130a73
Andy <q25a25q@live.com>
2020-04-16 16:54:07 +0800

    有效的括号

---
+++ Leetcode/Leetcode/stack/valid-parentheses.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Shouldly;
+using System.Collections.Generic;
+
+namespace Leetcode.stack
+{
+    /// <summary>
+    /// 有效的括号
+    /// </summary>
+    public class valid_parentheses
+    {
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("([)]", false)]
+        [InlineData("{[]}", true)]
+
+        public void 有效的括号(string input, bool result)
+        {
+            IsValid(input).ShouldBe(result);
+        }
+
+        public bool IsValid(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return true;
+            }
+            if (s.Length < 2)
+            {
+                return false;
+            }
+            var dic = new Dictionary<char, char>() {
+                { ')','(' },
+                { ']', '[' },
+                { '}', '{' },
+            };
+            var stack = new Stack<char>();
+            stack.Push(s[0]);
+            var left = 1;
+            while (left < s.Length)
+            {
+                if (stack.Count > 0 && dic.ContainsKey(s[left]))
+                {
+                    var top = stack.Peek();
+                    if (top == dic[s[left]])
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(s[left]);
+                    }
+                }
+                else
+                {
+                    stack.Push(s[left]);
+                }
+                left++;
+            }
+
+            return stack.Count == 0;
+        }
+
+    }
+}
