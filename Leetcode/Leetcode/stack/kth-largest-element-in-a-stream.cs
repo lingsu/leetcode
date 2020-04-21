00d972bd92857b4bb8d91d8d27056a1cfff3e402
Andy <q25a25q@live.com>
2020-04-21 11:24:11 +0800

    数据流中的第K大元素：小顶堆

--- Leetcode/Leetcode/stack/kth-largest-element-in-a-stream.cs
+++ Leetcode/Leetcode/stack/kth-largest-element-in-a-stream.cs
@@ -30,29 +30,111 @@ namespace Leetcode.stack
             obj.Add(9).ShouldBe(8);
         }
 
-        //最笨的方式
+        ////最笨的方式
+        //public class KthLargest
+        //{
+        //    private int k;
+        //    private int[] nums;
+        //    private List<int> list = new List<int>();
+        //    public KthLargest(int k, int[] nums)
+        //    {
+        //        this.k = k;
+        //        this.nums = nums;
+        //        foreach (var item in nums)
+        //        {
+        //            list.Add(item);
+        //        }
+        //    }
+
+        //    public int Add(int val)
+        //    {
+        //        list.Add(val);
+
+        //        return list.OrderByDescending(x=>x).ToList()[k -1];
+        //    }
+        //}
+
         public class KthLargest
         {
+            /// <summary>
+            /// 堆的规模
+            /// </summary>
             private int k;
-            private int[] nums;
-            private List<int> list = new List<int>();
+            /// <summary>
+            /// 小顶堆
+            /// </summary>
+            private int[] minHeap;
+            /// <summary>
+            /// 堆的最后一个元素的索引
+            /// </summary>
+            private int last;
             public KthLargest(int k, int[] nums)
             {
                 this.k = k;
-                this.nums = nums;
-                foreach (var item in nums)
+                minHeap = new int[k + 1];
+                //this.nums = nums;
+                for (int i = 1; i <= k && i <= nums.Length; i++)
                 {
-                    list.Add(item);
+                    minHeap[i] = nums[i - 1];
+                    last = i;
                 }
+                if (last == k)
+                {
+                    Order();
+                    for (int i = k; i < nums.Length; i++)
+                    {
+                        Add(nums[i]);
+                    }
+                }
+
             }
 
             public int Add(int val)
             {
-                list.Add(val);
-           
-                return list.OrderByDescending(x=>x).ToList()[k -1];
+                if (last < k)
+                {
+                    minHeap[++last] = val;
+                    Order();
+                }
+                else if (val > minHeap[1])
+                {
+                    minHeap[1] = val;
+                    Sink(1);
+                }
+                return minHeap[1];
+            }
+            /// <summary>
+            /// 堆的有序化
+            /// </summary>
+            private void Order()
+            {
+                for (int i = k / 2; i >= 1; i--)
+                {
+                    Sink(i);
+                }
+            }
+            private void Sink(int i)
+            {
+                while (2 * i <= k)
+                {
+                    var j = 2 * i;
+                    if (j < k && minHeap[j] > minHeap[j + 1])
+                    {
+                        j++;
+                    }
+                    if (minHeap[i] > minHeap[j])
+                    {
+                        var temp = minHeap[i];
+                        minHeap[i] = minHeap[j];
+                        minHeap[j] = temp;
+                        i = j;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
         }
-
     }
 }
