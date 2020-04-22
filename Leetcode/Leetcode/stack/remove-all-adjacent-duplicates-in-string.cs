07f3d0059ad98c5a62ba41cba7b49c2880229d85
Andy <q25a25q@live.com>
2020-04-22 09:06:38 +0800

    删除字符串中的所有相邻重复项

---
+++ Leetcode/Leetcode/stack/remove-all-adjacent-duplicates-in-string.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.stack
+{
+    /// <summary>
+    /// 删除字符串中的所有相邻重复项
+    /// </summary>
+    public class remove_all_adjacent_duplicates_in_string
+    {
+        [Fact]
+        public void 删除字符串中的所有相邻重复项()
+        {
+            RemoveDuplicates("abbaca").ShouldBe("ca");
+        }
+        public string RemoveDuplicates(string S)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var c in S)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Peek() != c)
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        while (stack.Count > 0 && stack.Peek() == c)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    
+                   
+                }
+            }
+            return string.Join("", stack.ToArray().Reverse());
+        }
+    }
+}
