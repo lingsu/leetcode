dbeb4090caa3fe13f4dfcf889da49c9be3f4d178
Andy <q25a25q@live.com>
2020-05-07 16:23:55 +0800

    判断子序列

---
+++ Leetcode/Leetcode/greedy/is-subsequence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+namespace Leetcode.greedy
+{
+    /// <summary>
+    /// 判断子序列
+    /// </summary>
+    public class is_subsequence
+    {
+        [Fact]
+        public void 判断子序列()
+        {
+            IsSubsequence("abc", "ahbgdc").ShouldBeTrue();
+            IsSubsequence("abc", "ahbgdc").ShouldBeTrue();
+        }
+        public bool IsSubsequence(string s, string t)
+        {
+            var i = -1;
+            foreach (var s1 in s)
+            {
+                i = t.IndexOf(s1, i + 1);
+                if (i == -1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    
+}
