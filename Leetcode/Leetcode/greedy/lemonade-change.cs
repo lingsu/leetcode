35174e024dc182a579a7e0f4c1ade3cec17bf547
Andy <q25a25q@live.com>
2020-05-08 10:49:21 +0800

    柠檬水找零

---
+++ Leetcode/Leetcode/greedy/lemonade-change.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.greedy
+{
+    /// <summary>
+    /// 柠檬水找零
+    /// </summary>
+    public class lemonade_change
+    {
+        [Fact]
+        public void 柠檬水找零()
+        {
+            LemonadeChange(new int[] { 5, 5, 5, 10, 20 }).ShouldBeTrue();
+            LemonadeChange(new int[] { 5, 5, 10, 10, 20 }).ShouldBeFalse();
+        }
+        public bool LemonadeChange(int[] bills)
+        {
+            if (bills == null || bills.Length == 0)
+            {
+                return false;
+            }
+
+            var five = 0;
+            var ten = 0;
+
+            foreach (var b in bills)
+            {
+                if (b == 5)
+                {
+                    five++;
+                }
+                else if (b == 10)
+                {
+                    if (five == 0)
+                    {
+                        return false;
+                    }
+                    five--;
+                    ten++;
+                }
+                else
+                {
+                    if (five > 0 && ten > 0)
+                    {
+                        ten--;
+                        five--;
+                    }
+                    else if (five >= 3)
+                    {
+                        five -= 3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
