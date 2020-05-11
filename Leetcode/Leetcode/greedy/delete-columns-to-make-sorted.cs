3b27be271d26a72b2b27157bdad6e8f52fe262c7
Andy <q25a25q@live.com>
2020-05-11 14:47:24 +0800

    删列造序

---
+++ Leetcode/Leetcode/greedy/delete-columns-to-make-sorted.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.greedy
+{
+    /// <summary>
+    /// 删列造序
+    /// </summary>
+    public class delete_columns_to_make_sorted
+    {
+        [Fact]
+        public void 删列造序()
+        {
+            MinDeletionSize(new string[] { "cba", "daf", "ghi" }).ShouldBe(1);
+            MinDeletionSize(new string[] { "a", "b" }).ShouldBe(0);
+            MinDeletionSize(new string[] { "zyx", "wvu", "tsr" }).ShouldBe(3);
+        }
+        public int MinDeletionSize(string[] A)
+        {
+            var nas = 0;
+
+            for (int i = 0; i < A[0].Length; i++)
+            {
+                for (int j = 1; j < A.Length; j++)
+                {
+                    if (A[j - 1][i] > A[j][i])
+                    {
+                        nas++;
+                        break;
+                    }
+                }
+            }
+
+            return nas;
+        }
+    }
+}
