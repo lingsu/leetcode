94722e65a9c7f861b954e2f8b0cdb7e2f3334176
Andy <q25a25q@live.com>
2020-06-04 10:36:30 +0800

    2的幂

---
+++ Leetcode/Leetcode/bit-manipulation/power-of-two.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.bit_manipulation
+{
+    /// <summary>
+    /// 2的幂
+    /// </summary>
+    public class power_of_two
+    {
+        //给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+        //示例 1:
+
+        //输入: 1
+        //输出: true
+        //解释: 20 = 1
+        //示例 2:
+
+        //输入: 16
+        //输出: true
+        //解释: 24 = 16
+        //示例 3:
+
+        //输入: 218
+        //输出: false
+
+        //来源：力扣（LeetCode）
+        //链接：https://leetcode-cn.com/problems/power-of-two
+        //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        [Fact]
+        public void two的幂()
+        {
+            IsPowerOfTwo(1).ShouldBeTrue();
+        }
+
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 0)
+            {
+                return false;
+            }
+
+            while (n % 2 == 0)
+            {
+                n /= 2;
+            }
+
+            return n == 1;
+        }
+    }
+}
