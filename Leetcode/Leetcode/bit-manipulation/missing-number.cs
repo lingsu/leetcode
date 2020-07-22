78347db1e7b75b4005bb67f9f8ccf4609775ccef
Andy <q25a25q@live.com>
2020-07-22 17:33:15 +0800

    缺失数字

---
+++ Leetcode/Leetcode/bit-manipulation/missing-number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.bit_manipulation
+{
+    /// <summary>
+    /// 缺失数字
+    /// </summary>
+    public class missing_number
+    {
+        [Fact]
+        public void 缺失数字()
+        {
+            MissingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }).ShouldBe(8);
+        }
+
+        public int MissingNumber(int[] nums)
+        {
+            var missing = nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                missing ^= i ^ nums[i];
+            }
+            return missing;
+        }
+    }
+}
