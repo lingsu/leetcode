1c131cc6ea5de6f8f7fe7c3dce2df7d7a187f9d1
Andy <q25a25q@live.com>
2020-07-24 14:47:00 +0800

    两整数之和:不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。

---
+++ Leetcode/Leetcode/bit-manipulation/sum-of-two-integers.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+
+namespace Leetcode.bit_manipulation
+{
+    /// <summary>
+    /// 两整数之和
+    /// </summary>
+    public class sum_of_two_integers
+    {
+        [Fact]
+        public void 两整数之和()
+        {
+            GetSum(1, 2).ShouldBe(3);
+        }
+        public int GetSum(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+            int lower;
+            int carrier;
+            while (true)
+            {
+                lower = a ^ b;
+                carrier = a & b;
+                if (carrier == 0)
+                {
+                    break;
+                }
+                a = lower;
+                b = carrier << 1;
+            }
+
+            return lower;
+        }
+    }
+}
