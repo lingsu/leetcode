58053a667f7cdb6e641b0c91c88b07144e47d18a
Andy <q25a25q@live.com>
2020-08-11 14:08:36 +0800

    子集2

---
+++ Leetcode/Leetcode/bit-manipulation/sbusets2.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace Leetcode.bit_manipulation
+{
+    public class sbusets2
+    {
+        private IList<IList<int>> res = new List<IList<int>>();
+        [Fact]
+        public void 子集2()
+        {
+            //var s = SubsetsWithDup(new int[] { 1, 2, 2 });
+            //s.Count.ShouldBe(6);
+
+            var s = SubsetsWithDup(new int[] { 4, 4, 4, 1, 4 });
+            s.Count.ShouldBe(10);
+        }
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            //先排序，这样相同的两个元素必相邻
+            Array.Sort(nums);
+            backtrack(nums, new List<int>(), 0);
+            return res;
+        }
+        private void backtrack(int[] nums, List<int> path, int start)
+        {
+            res.Add(path);
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i > start && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                var newPath = new List<int>();
+                newPath.AddRange(path);
+                newPath.Add(nums[i]);
+                backtrack(nums, newPath, i + 1);
+            }
+        }
+    }
+}
