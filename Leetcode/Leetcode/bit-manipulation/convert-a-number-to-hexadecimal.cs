63accc31758b2dbaf8ecf73a3b578ed2240555f4
Andy <q25a25q@live.com>
2020-08-13 16:17:15 +0800

    数字转换为十六进制数

---
+++ Leetcode/Leetcode/bit-manipulation/convert-a-number-to-hexadecimal.cs
@@ -0,0 +1,40 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace Leetcode.bit_manipulation
+{
+    /// <summary>
+    /// 数字转换为十六进制数
+    /// </summary>
+    public class convert_a_number_to_hexadecimal
+    {
+        [Fact]
+        public void 数字转换为十六进制数()
+        {
+
+            ToHex(0).ShouldBe("0");
+            ToHex(26).ShouldBe("1a");
+            ToHex(-1).ShouldBe("ffffffff");
+        }
+        public string ToHex(int num)
+        {
+            var chars = "0123456789abcdef";
+            var sb = new StringBuilder();
+            var cnt = 8;
+            do
+            {
+                var lowbit4 = num & 0xf;
+                sb.Append(chars[lowbit4]);
+                num >>= 4;
+                cnt--;
+            } while (num != 0 && cnt > 0);
+
+
+            return string.Join("", sb.ToString().Reverse());
+        }
+    }
+}
