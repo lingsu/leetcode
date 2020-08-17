c5933f617c963aa79ec79dedbfe3bc56fdbaf54b
Andy <q25a25q@live.com>
2020-08-17 15:56:18 +0800

    将数字变成0的操作次数

---
+++ Leetcode/Leetcode/bit-manipulation/number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,37 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leetcode.bit_manipulation
+{
+    public class number_of_steps_to_reduce_a_number_to_zero
+    {
+        [Theory]
+        [InlineData(14,6)]
+        public void 将数字变成0的操作次数(int num,int result)
+        {
+            NumberOfSteps(num).ShouldBe(result);
+        }
+        public int NumberOfSteps(int num)
+        {
+            var res = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num >>= 1;
+                }
+                else
+                {
+                    num = num - 1;
+                }
+                res++;
+            }
+
+            return res;
+        }
+    }
+}
