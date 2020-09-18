55b9483ea688dcc8d1d42331ebb6cbf1a0cc8b53
andy <q25a25q@live.com>
2020-09-18 15:48:30 +0800

    路径总和:给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。

---
+++ Leetcode/Leetcode/tree/path-sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leetcode.tree
+{
+    /// <summary>
+    /// 路径总和
+    /// </summary>
+    public class path_sum
+    {
+        [Fact]
+        public void 路径总和()
+        {
+
+        }
+        //给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+        //说明: 叶子节点是指没有子节点的节点。
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.left == null && root.right == null)
+            {
+                return root.val == sum;
+            }
+
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+
+    }
+}
