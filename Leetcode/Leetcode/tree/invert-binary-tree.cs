9a17a9316595c96d34db4c670fe4021de1c56e67
andy <q25a25q@live.com>
2020-09-21 09:37:04 +0800

    翻转二叉树

---
+++ Leetcode/Leetcode/tree/invert-binary-tree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leetcode.tree
+{
+    /// <summary>
+    /// 翻转二叉树
+    /// </summary>
+    public class invert_binary_tree
+    {
+        [Fact]
+        public void 翻转二叉树()
+        {
+
+        }
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            var left = root.left;
+            root.left = root.right;
+            root.right = left;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
