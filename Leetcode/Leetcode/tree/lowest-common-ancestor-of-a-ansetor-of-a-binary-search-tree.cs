b848c8a3278b52c5c1f297ad69a453eaa26da26d
andy <q25a25q@live.com>
2020-09-21 10:36:19 +0800

    二叉搜索树的最近公共祖先

---
+++ Leetcode/Leetcode/tree/lowest-common-ancestor-of-a-ansetor-of-a-binary-search-tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leetcode.tree
+{
+    /// <summary>
+    /// 二叉搜索树的最近公共祖先
+    /// </summary>
+    public class lowest_common_ancestor_of_a_ansetor_of_a_binary_search_tree
+    {
+        [Fact]
+        public void 二叉搜索树的最近公共祖先()
+        {
+
+        }
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            var pv = p.val;
+            var qv = q.val;
+
+            if (pv > root.val && qv > root.val)
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+            else if (pv < root.val && qv < root.val)
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+
+            return root;
+        }
+    }
+}
