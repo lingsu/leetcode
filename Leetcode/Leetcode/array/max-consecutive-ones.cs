636e6e971049b9f9740233c4640185366fdea6d6
andy <q25a25q@live.com>
2020-10-16 09:24:13 +0800

    最大连续1的个数

---
+++ Leetcode/Leetcode/array/max-consecutive-ones.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leetcode.array
+{
+    /// <summary>
+    /// 最大连续1的个数
+    /// </summary>
+    public class max_consecutive_ones
+    {
+        [Fact]
+        public void 最大连续1的个数()
+        {
+            FindMaxConsecutiveOnes(new[] { 1, 1, 0, 1, 1, 1 }).ShouldBe(3);
+        }
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var max = int.MinValue;
+            var count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        if (count > max)
+                        {
+                            max = count;
+                        }
+                        count = 0;
+                    }
+                }
+            }
+            if (count > max)
+            {
+                max = count;
+            }
+            return max;
+        }
+    }
+}
